3f56cf2c4778c9c59d931b455bb931bc82ac358e
Kir2023 <132088682+Kir2023@users.noreply.github.com>
2024-05-03 18:49:22 +0300

    add ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -10,11 +8,9 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+link        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+link        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
